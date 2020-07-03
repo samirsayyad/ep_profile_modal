d7249a20d2f13c145f21972488ac48566f786dfb
Samir Sayyad <samir.saiad@gmail.com>
2020-07-03 17:07:48 +0430

    open/close like etherpad animation

--- static/js/profileModal.js
+++ static/js/profileModal.js
@@ -1,9 +1,11 @@
 exports.postAceInit = function (hook,context){
 	var hs = $('#ep-profile-button');
 	hs.on('click', function(){
-		($('#ep_profile_modal').is(":visible")) ? 
-		$('#ep_profile_modal').animate({ opacity: 0 }) : $('#ep_profile_modal').animate({ opacity: 1 })
-		
+		($('#ep_profile_modal').hasClass('ep_profile_modal-show'))?
+		$('#ep_profile_modal').removeClass('ep_profile_modal-show')
+		:
+		$('#ep_profile_modal').addClass('ep_profile_modal-show')
+
   	});
 }
 
