8461b39d3804a75061287209b3c0fd473ba10125
Samir Sayyad <samir.saiad@gmail.com>
2020-07-03 18:03:20 +0430

    add gravatar

--- static/js/initialize.js
+++ static/js/initialize.js
@@ -1,7 +1,7 @@
 var gravatar = require('gravatar');
 
 exports.aceInitialized = function(hook, context){
-    var httpsUrl = gravatar.url('emerleite@gmail.com', {protocol: 'https', s: '50'});
+    var httpsUrl = gravatar.url('samir.saiad@gmail.com', {protocol: 'https', s: '50'});
 
     $("body").append("<div class='ep_profile_modal' id='ep_profile_modal'></div>")
     $("#pad_title").append("<div id='ep-profile-button'><img src='"+httpsUrl+"' /></div>")
