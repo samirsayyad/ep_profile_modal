55091d03e6a461fb18ed66e7125bb3555e6dd848
Samir Sayyad <samir.saiad@gmail.com>
2020-07-03 20:51:49 +0430

    gravatar test

--- profile.js
+++ profile.js
@@ -1,7 +1,17 @@
 var eejs = require('ep_etherpad-lite/node/eejs/');
+var gravatar = require('gravatar');
 
 exports.eejsBlock_styles = function (hook_name, args, cb) {
     args.content = args.content + eejs.require("ep_profile_modal/templates/styles.html", {}, module);
     return cb();
 }
 
+exports.clientVars = function  (hook, context, callback){
+    var httpsUrl = gravatar.url('samir.saiad@gmail.com', {protocol: 'https', s: '50'});
+    console.log(httpsUrl , "here")
+    return callback({
+        ep_profile_modal: {
+            profile_image_url: httpsUrl
+        }
+    });
+}
--- static/js/initialize.js
+++ static/js/initialize.js
@@ -1,8 +1,6 @@
-var gravatar = require('gravatar');
 
 exports.aceInitialized = function(hook, context){
-    var httpsUrl = gravatar.url('samir.saiad@gmail.com', {protocol: 'https', s: '50'});
-
+    console.log(clientVars)
     $("body").append("<div class='ep_profile_modal' id='ep_profile_modal'></div>")
-    $("#pad_title").append("<div id='ep-profile-button'><img src='"+httpsUrl+"' /></div>")
+    $("#pad_title").append("<div id='ep-profile-button'><img src='"+clientVars.ep_profile_modal.profile_image_url+"' /></div>")
 }
