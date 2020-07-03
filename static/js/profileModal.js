85f7af85c61d131f0348da151cbeba23992ca10b
Samir Sayyad <samir.saiad@gmail.com>
2020-07-03 16:41:13 +0430

    styling on modal

--- static/js/profileModal.js
+++ static/js/profileModal.js
@@ -1,7 +1,9 @@
 exports.postAceInit = function (hook,context){
 	var hs = $('#ep-profile-button');
 	hs.on('click', function(){
-		$('#ep_profile_modal').show()
+		($('#ep_profile_modal').is(":visible")) ? 
+		$('#ep_profile_modal').animate({ opacity: 0 }) : $('#ep_profile_modal').animate({ opacity: 1 })
+		
   	});
 }
 
