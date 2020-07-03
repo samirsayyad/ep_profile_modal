dae54daec27cdb32cd53965b56c0cc0fadf217e5
Samir Sayyad <samir.saiad@gmail.com>
2020-07-03 17:52:48 +0430

    going to toolbar

--- profile.js
+++ profile.js
@@ -1,16 +1,7 @@
 var eejs = require('ep_etherpad-lite/node/eejs/');
 
-exports.eejsBlock_editbarMenuRight = function (hook_name,args,cb){
-	args.content += eejs.require('ep_profile_modal/templates/profileButton.ejs');
-	return cb();
-
-}
 exports.eejsBlock_styles = function (hook_name, args, cb) {
     args.content = args.content + eejs.require("ep_profile_modal/templates/styles.html", {}, module);
     return cb();
 }
 
-exports.eejsBlock_body = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_profile_modal/templates/modal.html", {}, module);
-  return cb();
-}
--- static/js/initialize.js
+++ static/js/initialize.js
@@ -1,2 +1,3 @@
 exports.aceInitialized = function(hook, context){
+    $("$pad_title").append("<div class='ep_profile_modal' id='ep_profile_modal'>Profile</div>")
 }
