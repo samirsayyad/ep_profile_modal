1488d384069e5e687b686be489396f4ba024a0ac
Samir Sayyad <samir.saiad@gmail.com>
2020-07-08 15:30:59 +0430

    vars

--- profile.js
+++ profile.js
@@ -1,5 +1,8 @@
 var eejs = require('ep_etherpad-lite/node/eejs/');
 var gravatar = require('gravatar');
+var httpsUrl = gravatar.url('samir.saiad@gmail.com', {protocol: 'https', s: '100'});
+var profile_url = gravatar.profile_url('samir.saiad@gmail.com', {protocol: 'https' });
+
 
 exports.eejsBlock_styles = function (hook_name, args, cb) {
     args.content = args.content + eejs.require("ep_profile_modal/templates/styles.html", {}, module);
@@ -7,15 +10,12 @@ exports.eejsBlock_styles = function (hook_name, args, cb) {
 }
 
 exports.eejsBlock_body = function (hook_name, args, cb) {
-    args.content = args.content + eejs.require("ep_profile_modal/templates/profileModal.html", {}, module);
+    args.content = args.content + eejs.require("ep_profile_modal/templates/profileModal.html", {profile_image_url: httpsUrl}, module);
     return cb();
 }
 
 exports.clientVars = function  (hook, context, callback){
-    var httpsUrl = gravatar.url('samir.saiad@gmail.com', {protocol: 'https', s: '100'});
-    var profile_url = gravatar.profile_url('samir.saiad@gmail.com', {protocol: 'https' });
 
-    
     console.log(httpsUrl , "here")
     return callback({
         ep_profile_modal: {
