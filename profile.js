def91fff6681bcd74182d72b67c062733b64d54b
Samir Sayyad <samir.saiad@gmail.com>
2020-07-08 13:45:36 +0430

    profile gravatar

--- profile.js
+++ profile.js
@@ -7,11 +7,15 @@ exports.eejsBlock_styles = function (hook_name, args, cb) {
 }
 
 exports.clientVars = function  (hook, context, callback){
-    var httpsUrl = gravatar.url('samir.saiad@gmail.com', {protocol: 'https', s: '50'});
+    var httpsUrl = gravatar.url('samir.saiad@gmail.com', {protocol: 'https', s: '100'});
+    var profile_url = gravatar.profile_url('samir.saiad@gmail.com', {protocol: 'https' });
+
+    
     console.log(httpsUrl , "here")
     return callback({
         ep_profile_modal: {
-            profile_image_url: httpsUrl
+            profile_image_url: httpsUrl,
+            profile_url : profile_url
         }
     });
 }
