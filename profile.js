43f6cd568cc48a522c9fd35075d285c8056ae5f2
Samir Sayyad <samir.saiad@gmail.com>
2020-07-09 14:04:37 +0430

    ask modal and img

--- profile.js
+++ profile.js
@@ -1,26 +1,43 @@
 var eejs = require('ep_etherpad-lite/node/eejs/');
 var gravatar = require('gravatar');
-var httpsUrl = gravatar.url('samir.saiad@gmail.com', {protocol: 'https', s: '100'});
-var profile_url = gravatar.profile_url('samir.saiad@gmail.com', {protocol: 'https' });
-
+const fetch = require('node-fetch');
+var user_email = 'asamir.saiad@gmail.com' ;
+var httpsUrl = gravatar.url(user_email, {protocol: 'https', s: '200'});
+var profile_url = gravatar.profile_url(user_email, {protocol: 'https' });
+var profile_json = null;
 
 exports.eejsBlock_styles = function (hook_name, args, cb) {
     args.content = args.content + eejs.require("ep_profile_modal/templates/styles.html", {}, module);
     return cb();
 }
 
-exports.eejsBlock_body = function (hook_name, args, cb) {
-    args.content = args.content + eejs.require("ep_profile_modal/templates/profileModal.html", {profile_image_url: httpsUrl}, module);
+exports.eejsBlock_scripts = function (hook_name, args, cb) {
+    args.content = args.content + eejs.require("ep_profile_modal/templates/profileModal.html", {}, module);
+    args.content += eejs.require("ep_profile_modal/templates/askModal.html", {}, module);
+
     return cb();
 }
 
-exports.clientVars = function  (hook, context, callback){
+exports.clientVars = async function  (hook, context, callback){
 
-    console.log(httpsUrl , "here")
+    //console.log(httpsUrl , "here")
+    
+
+    profile_json = await fetch(profile_url) ;
+    profile_json = await profile_json.json()
+    console.log(profile_json  +  " jjjjjjjj")
+
+    if (profile_json !="User not found")
+        profile_json = profile_json.entry[0]
+    else
+        profile_json = null 
+    //console.log(profile_json.entry[0] +  " jjjjjjjj")
     return callback({
         ep_profile_modal: {
             profile_image_url: httpsUrl,
-            profile_url : profile_url
+            profile_json : profile_json  ,
+            user_email : user_email
         }
     });
 }
+
--- static/js/initialize.js
+++ static/js/initialize.js
@@ -1,6 +1,15 @@
 
 exports.aceInitialized = function(hook, context){
-    console.log(clientVars)
-    //$("body").append("<div class='ep_profile_modal' id='ep_profile_modal'></div>")
-    $("#pad_title").append("<div id='ep-profile-button'><img id='ep-profile-image'  src='"+clientVars.ep_profile_modal.profile_image_url+"' /></div>")
+    console.log(clientVars.ep_profile_modal)
+    if(clientVars.profile_json == null){
+        $("#pad_title").append("<div id='ep-profile-button'><img id='ep-profile-image'  src='../static/plugins/ep_profile_modal/static/img/user.png' /></div>")
+        var modal = $("#ep_profile_askmodal_script").tmpl(clientVars.ep_profile_modal);
+        $("body").append(modal)
+    }else{
+        $("#pad_title").append("<div id='ep-profile-button'><img id='ep-profile-image'  src='"+clientVars.ep_profile_modal.profile_image_url+"' /></div>")
+        var modal = $("#ep_profile_modal_script").tmpl(clientVars.ep_profile_modal);
+        $("body").append(modal)
+    }
+    
+    
 }
