373194eba076425c77ffeed6544134c85d903a85
Samir Sayyad <samir.saiad@gmail.com>
2020-09-23 18:49:44 +0330

    change all over img

--- static/js/initialize.js
+++ static/js/initialize.js
@@ -18,7 +18,7 @@ exports.aceInitialized = function(hook, context){
     if(clientVars.ep_profile_modal.user_status == "2"){
       
         window.user_status = "login";
-        $("#pad_title").append("<div class='ep_profile_modal_header'><div class='userlist' id='userlist'>"+usersListHTML+"</div><div class='ep-profile-button' id='ep-profile-button'><img id='ep-profile-image'  src='"+clientVars.ep_profile_modal.profile_image_url+"' /></div></div>")
+        $("#pad_title").append("<div class='ep_profile_modal_header'><div class='userlist' id='userlist'>"+usersListHTML+"</div><div class='ep-profile-button' id='ep-profile-button'><img id='ep-profile-image'  src='/p/getUserProfileImage/"+clientVars.userId+"' /></div></div>")
         
     }else{
         $("#pad_title").append("<div class='ep_profile_modal_header'><div class='userlist' id='userlist'>"+usersListHTML+" </div><div class='ep-profile-button' id='ep-profile-button'><img id='ep-profile-image'  src='../static/plugins/ep_profile_modal/static/img/user.png' /></div></div>")
