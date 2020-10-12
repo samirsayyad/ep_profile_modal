92d034be1e47e616a427ed0cb5c7c6b06b97c7d9
Samir Sayyad <samir.saiad@gmail.com>
2020-10-12 10:45:33 +0330

    add enter key listener

--- static/js/profileForm/main.js
+++ static/js/profileForm/main.js
@@ -14,77 +14,24 @@ exports.initModal = function(clientVars){
         var left, opacity, scale; //fieldset properties which we will animate
         var animating; //flag to prevent quick multi-click glitches
 
+
+
+        $("#ep_profile_formModal_msform fieldset").on("keypress",function(e){
+            if (e.keyCode == 13) {
+
+                // Cancel the default action on keypress event
+                e.preventDefault(); 
+                current_fs = $(this);
+                next_fs = $(this).next();
+                nextHandler(current_fs,next_fs)
+
+            }
+        })
         $(".next").click(function(){
-            console.log("clicked")
-            if(animating) return false;
             current_fs = $(this).parent();
             next_fs = $(this).parent().next();
-            var currentSection = current_fs.attr("data-section")
-            if (currentSection=="name"){
-                if ($("#ep_profile_modalForm_name").val() == "") {
-                    $("#ep_profile_modalForm_name").css({"border":"1px solid red"})
-                    return false;
-                }
-                $("#ep_profile_modalForm_name").css({"border":"1px solid gray"})
-
-            }
-            if (currentSection=="email"){
-                var userEmail = $("#ep_profile_modalForm_email").val()
-                if (!shared.isEmail(userEmail) || userEmail==""){
-                    $("#ep_profile_modalForm_email").css({"border":"1px solid red"})
-                    return false;
-                }
-                $("#ep_profile_modalForm_email").css({"border":"1px solid gray"})
-            }
-
-            if (currentSection=="homepage"){
-                var userLink = $("#ep_profile_modal_homepage").val()
-                console.log(shared.IsValid(userLink))
-                if (!shared.IsValid(userLink) || userLink==""){
-                    $("#ep_profile_modal_homepage").css({"border":"1px solid red"})
-                    return false;
-                }
-                $("#ep_profile_modal_homepage").css({"border":"1px solid gray"})
-            }
-
-            animating = true;
-
+            nextHandler(current_fs,next_fs)
             
-            //activate next step on progressbar using the index of next_fs
-            //$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-            
-            //show the next fieldset
-            current_fs.hide();
-            next_fs.show(); 
-            // next_fs.addClass("ep_profile_formModal_show")
-
-            // current_fs.removeClass("ep_profile_formModal_show")
-//            next_fs.addClass("ep_profile_formModal_show")
-            animating = false;
-            //hide the current fieldset with style
-            // current_fs.animate({opacity: 0}, {
-            //     step: function(now, mx) {
-            //         //as the opacity of current_fs reduces to 0 - stored in "now"
-            //         //1. scale current_fs down to 80%
-            //         scale = 1 - (1 - now) * 0.2;
-            //         //2. bring next_fs from the right(50%)
-            //         left = (now * 50)+"%";
-            //         //3. increase opacity of next_fs to 1 as it moves in
-            //         opacity = 1 - now;
-            //         current_fs.css({
-            //     'transform': 'scale('+scale+')',
-            //     'position': 'absolute'
-            // });
-            //         next_fs.css({'left': left, 'opacity': opacity});
-            //     }, 
-            //     duration: 800, 
-            //     complete: function(){
-            //         current_fs.hide();
-            //         animating = false;
-            //     }, 
-            //     //this comes from the custom easing plugin
-            //     easing: 'easeInOutBack'
-            // });
         });
 
         $(".skip").click(function(){
@@ -176,6 +123,13 @@ exports.initModal = function(clientVars){
         });
 
         $(".submit").click(function(){
+            submitHandle()
+            return false;
+        })
+        $(".clear").click(function(){
+            shared.resetAllProfileImage($(this).attr("data-userId"),$(this).attr("data-padId"))
+        })
+        function submitHandle (){
             var userId = pad.getUserId() 
             var padId =  pad.getPadId()
             $('#ep_profile_formModal').removeClass('ep_profile_formModal_show')
@@ -204,21 +158,60 @@ exports.initModal = function(clientVars){
             }, 2200);
             // sync profile section to up 
             
-            return false;
-        })
-        $(".clear").click(function(){
-            shared.resetAllProfileImage($(this).attr("data-userId"),$(this).attr("data-padId"))
-        })
+        }
 
-}
+        function getFormData($form){
+            var unindexed_array = $form.serializeArray();
+            var indexed_array = {};
 
-function getFormData($form){
-    var unindexed_array = $form.serializeArray();
-    var indexed_array = {};
+            $.map(unindexed_array, function(n, i){
+                indexed_array[n['name']] = n['value'];
+            });
 
-    $.map(unindexed_array, function(n, i){
-        indexed_array[n['name']] = n['value'];
-    });
+            return indexed_array;
+        }
 
-    return indexed_array;
+        function nextHandler(current_fs,next_fs){
+            console.log("clicked")
+            if(animating) return false;
+            animating = true;
+
+            var currentSection = current_fs.attr("data-section")
+            if (currentSection=="name"){
+                if ($("#ep_profile_modalForm_name").val() == "") {
+                    $("#ep_profile_modalForm_name").css({"border":"1px solid red"})
+                    return false;
+                }
+                $("#ep_profile_modalForm_name").css({"border":"1px solid gray"})
+
+            }
+            if (currentSection=="email"){
+                var userEmail = $("#ep_profile_modalForm_email").val()
+                if (!shared.isEmail(userEmail) || userEmail==""){
+                    $("#ep_profile_modalForm_email").css({"border":"1px solid red"})
+                    return false;
+                }
+                $("#ep_profile_modalForm_email").css({"border":"1px solid gray"})
+            }
+
+            if (currentSection=="homepage"){
+                var userLink = $("#ep_profile_modal_homepage").val()
+                console.log(shared.IsValid(userLink))
+                if (!shared.IsValid(userLink) || userLink==""){
+                    $("#ep_profile_modal_homepage").css({"border":"1px solid red"})
+                    return false;
+                }
+                $("#ep_profile_modal_homepage").css({"border":"1px solid gray"})
+            }
+
+            animating = true;
+            current_fs.hide();
+            if (next_fs.length){
+                next_fs.show(); 
+            }else{ //seems last fieldset
+                submitHandle()
+            }
+            animating = false;
+        }
+
 }
